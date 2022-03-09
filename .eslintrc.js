f11134e8e4d04281628eadc354db689255922688
somq14 <som.q14@gmail.com>
2022-03-09 23:33:46 +0900

    refactor: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
-/* eslint-env node */
 module.exports = {
   root: true,
   extends: ["@somq14/eslint-config"],
-  parserOptions: {
-    project: ["./tsconfig.eslint.json"],
-  },
-  ignorePatterns: ["dist", "coverage", ".eslintrc.js"],
+  ignorePatterns: ["dist", "coverage"],
 };
